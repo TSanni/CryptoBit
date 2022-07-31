95ad0d3c9d93ad98b0b806ba1045bcf70b9d4698
TSanni <tomas.sanni@gmail.com>
2022-07-31 04:10:19 -0500

    Initial Commit

---
+++ CryptoBit/CryptoBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBitApp.swift
+//  CryptoBit
+//
+//  Created by Tomas Sanni on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
